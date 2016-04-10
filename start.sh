b4ba55c27740fd98055bcb1476bf92432299353c
cherry4477 <cherry4477@users.noreply.github.com>
2016-04-10 17:09:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ DATA=/data
 cp /app/nginx/nginx.conf ${NGINX}/conf/nginx.conf
 cp /app/nginx/docker-registry.default ${NGINX}/conf/docker-registry.default
 
-sed -i 's/registry/'$servicename'/g' /usr/local/nginx/conf/docker-registry.default
-sed -i 's/5000/'$serviceport'/g' /usr/local/nginx/conf/docker-registry.default
-sed -i 's/ldap:\\/\\/54.222.156.183:40389\\/ou=Users,dc=openstack,dc=org?uid?sub?(objectClass=inetOrgPerson)/'$ldap'/g' ${NGINX}/conf/docker-registry.default
+sed -i 's/#registry#/'$servicename'/g' ${NGINX}/conf/docker-registry.default
+sed -i 's/#5000#/'$serviceport'/g' ${NGINX}/conf/docker-registry.default
+sed -i 's/#ldap://54.222.156.183:40389/ou=Users,dc=openstack,dc=org?uid?sub?(objectClass=inetOrgPerson)#/'$ldap'/g' ${NGINX}/conf/docker-registry.default
 
 if [ ! -f ${DATA}/server.crt ];
 then
