ddb2737bec4ca196f09c5aac146e2904b8859cbf
cherry4477 <cherry4477@users.noreply.github.com>
2016-04-10 16:18:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ DATA=/data
 cp /app/nginx/nginx.conf ${NGINX}/conf/nginx.conf
 cp /app/nginx/docker-registry.default ${NGINX}/conf/docker-registry.default
 
+sed -i 's/registry:5000/'$servicename:$serviceport'/g' ${NGINX}/conf/docker-registry.default
+sed -i 's/ldap://54.222.156.183:40389/ou=Users,dc=openstack,dc=org?uid?sub?(objectClass=inetOrgPerson)/'$ldap'/g'
+
 if [ ! -f ${DATA}/server.crt ];
 then
     echo "use default server certification for demo purpose !!"
